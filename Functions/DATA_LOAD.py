386b11e8605f8bc221b0d3f8c1a95f9396c1b6cf
Mina Makram <mina.makram.riad.111@gmail.com>
2026-08-07 23:01:09 +0300

    Try to match columns names

---
+++ Functions/DATA_LOAD.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def load_excel(file_path: str):
+    """
+    Loads an Excel file into a pandas DataFrame.
+    """
+    try:
+        df = pd.read_excel(file_path)
+
+    except Exception as e:
+        print(f"Error loading Excel file: {e}")
+        return pd.DataFrame()
--- Functions/__init__.py
+++ Functions/__init__.py
@@ -1,2 +1,2 @@
-from .LOAD_AND_DATE_CLEAN import load_excel
+from .DATA_LOAD import load_excel
 from .Clean_Column_Names import preprocess_header, clean_and_align_columns
